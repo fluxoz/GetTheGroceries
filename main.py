1b38dfd503bad2a14e5893ab5d80a4cadac3a89a
fluxoz <18295572+fluxoz@users.noreply.github.com>
2020-06-29 01:45:55 -0700

    update to add_recipe

--- main.py
+++ main.py
@@ -283,7 +283,7 @@ def edit_recipe():
             return redirect(url_for('dashboard'))
         else:
             flash('The title "' + newtitle + '" has already been used by you, pick a different title.', 'danger')
-            return render_template('edit_recipe.html', form=form, ingredients=ingredients, amounts=amounts, units=units,unittype=unittype)
+            return render_template('edit_recipe.html', form=form, ingredients=ingredients, amounts=amounts, units=units, unittype=unittype)
     else:
         my_sql_close(connection,cursor)
         return render_template('edit_recipe.html', form=form, ingredients=ingredients, amounts=amounts, units=units, unittype=unittype)
