4209375eed447716e2980ae4f2dd6e513605456e
fluxoz <18295572+fluxoz@users.noreply.github.com>
2020-07-06 17:39:50 -0700

    adding log output

--- static/js/add_ingredient.js
+++ static/js/add_ingredient.js
@@ -5,7 +5,7 @@ function uuidv4() {
   }
 
 function ingredient_fields() {
-    var ingrs_exist = document.getElementsById("ingredient_body").innerHTML ? true: false;
+    var ingrs_exist = document.getElementById("ingredient_body").innerHTML ? true: false;
     console.log(ingrs_exist);
     var button = null;
     if (ingrs_exist) {
