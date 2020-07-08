d2bee100ed3268b10d885553dc3bf93aa529d0e0
fluxoz <18295572+fluxoz@users.noreply.github.com>
2020-07-08 16:05:41 -0700

    fading alert after 2 seconds.

--- static/js/ingredients.js
+++ static/js/ingredients.js
@@ -67,7 +67,7 @@ function remove_ingredient_fields(rid) {
         var buffer = document.getElementById("message-buffer");
         buffer.innerHTML = `<div class="alert alert-danger">Recipes must have at least one ingredient.</div>`;
         setTimeout(function(){
-            $('.alert').delay(2000).fadeOut();
+            $('.alert').fadeOut();
         }, 2000);
     }
 }
