4eec384919c0d7ca2750ffb5094ffbc420f4582a
fluxoz <18295572+fluxoz@users.noreply.github.com>
2020-07-08 14:13:37 -0700

    updated edit_recipe JS

--- static/js/ingredients.js
+++ static/js/ingredients.js
@@ -45,7 +45,7 @@ function ingredient_fields(ingredient=false, amount=false, unit=false) {
     var select = document.getElementById(uuid + "_select");
     var unittype = ['kg', 'g', 'lb', 'oz', 'L', 'mL', 'Tblsp', 'tsp', 'cup', 'quart', 'gallon', 'package', 'jar', 'qty']
     console.log(unittype[unit_value]);
-    unittype.forEach(function(element, key, unittype, unit_value) {
+    unittype.forEach(function(element, key, unit_value) {
         if (element === unittype[unit_value]) {
             console.log("Selected Option BRAH");
             select[key] = new Option(element,key, true, true);
